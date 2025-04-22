f72de55a0de982b60012eb1ef61a77b4f1d6a423
Manish Karmakar <mrinalkarmakar565@gmail.com>
2025-04-22 21:41:15 +0530

    Initial Commit

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -1,28 +1,40 @@
-const baseURL = "demo.once-ui.com";
+const baseURL = "demo.magic-portfolio.com";
 
-// default customization applied to the HTML in the main layout.tsx
+const routes = {
+  "/": true,
+  "/about": true,
+  "/work": true,
+  "/blog": true,
+  "/gallery": true,
+};
+
+// Enable password protection on selected routes
+// Set password in the .env file, refer to .env.example
+const protectedRoutes = {
+  "/work/automate-design-handovers-with-a-figma-to-code-pipeline": true,
+};
+
 const style = {
-  theme: "dark", // dark | light - not needed when using ThemeProvider
+  theme: "dark", // dark | light
   neutral: "gray", // sand | gray | slate
-  brand: "blue", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
-  accent: "indigo", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
-  solid: "contrast", // color | contrast | inverse
+  brand: "emerald", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+  accent: "orange", // blue | indigo | violet | magenta | pink | red | orange | yellow | moss | green | emerald | aqua | cyan
+  solid: "contrast", // color | contrast
   solidStyle: "flat", // flat | plastic
   border: "playful", // rounded | playful | conservative
-  surface: "filled", // filled | translucent
+  surface: "translucent", // filled | translucent
   transition: "all", // all | micro | macro
-  scaling: "100", // 90 | 95 | 100 | 105 | 110
 };
 
 const effects = {
   mask: {
-    cursor: false,
-    x: 50,
+    cursor: true,
+    x: 0,
     y: 0,
-    radius: 100,
+    radius: 75,
   },
   gradient: {
-    display: false,
+    display: true,
     x: 50,
     y: 0,
     width: 100,
@@ -46,40 +58,52 @@ const effects = {
   grid: {
     display: false,
     color: "neutral-alpha-weak",
-    width: "24",
-    height: "24",
     opacity: 100,
   },
 };
 
-// default metadata
-const meta = {
-  title: "Once UI for Next.js",
-  description:
-    "An open-source design system and component library for Next.js that emphasizes easy styling and accessibility in UI development.",
+const display = {
+  location: true,
+  time: true,
 };
 
-// default open graph data
-const og = {
-  title: meta.title,
-  description: meta.description,
-  image: "/images/cover.jpg",
+const mailchimp = {
+  action: "https://url/subscribe/post?parameters",
+  effects: {
+    mask: {
+      cursor: false,
+      x: 100,
+      y: 0,
+      radius: 100,
+    },
+    gradient: {
+      display: true,
+      x: 100,
+      y: 50,
+      width: 100,
+      height: 100,
+      tilt: -45,
+      colorStart: "accent-background-strong",
+      colorEnd: "static-transparent",
+      opacity: 100,
+    },
+    dots: {
+      display: false,
+      size: 24,
+      color: "brand-on-background-weak",
+      opacity: 100,
+    },
+    lines: {
+      display: false,
+      color: "neutral-alpha-weak",
+      opacity: 100,
+    },
+    grid: {
+      display: true,
+      color: "neutral-alpha-weak",
+      opacity: 100,
+    },
+  },
 };
 
-// default schema data
-const schema = {
-  logo: "",
-  type: "Organization",
-  name: "Once UI",
-  description: meta.description,
-  email: "lorant@once-ui.com",
-};
-
-// social links
-const social = {
-  twitter: "https://www.twitter.com/_onceui",
-  linkedin: "https://www.linkedin.com/company/once-ui/",
-  discord: "https://discord.com/invite/5EyAQ4eNdS",
-};
-
-export { baseURL, style, meta, og, schema, social, effects };
+export { routes, protectedRoutes, effects, style, display, mailchimp, baseURL };
